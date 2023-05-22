a1647432b39f0ba9517eb9886a523e7fc5b9ffd2
issamaro <aissa.c@outlook.fr>
2023-05-22 23:38:16 +0200

    MyAgilos V0.8.5

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,5 +5,4 @@ set -e
 python manage.py wait_for_db
 python manage.py migrate
 python manage.py collectstatic --no-input
-sass /vol/web/static/style.scss /vol/static/style.css
 uwsgi --socket :8000 --master --enable-threads --module agilos.wsgi
