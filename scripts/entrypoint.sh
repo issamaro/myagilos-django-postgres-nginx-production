39e2ac00174f89a580642da3454a662b038987c5
issamaro <aissa.c@outlook.fr>
2023-05-24 15:14:37 +0200

    Test

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,6 @@
 set -e
 
 python manage.py wait_for_db
-python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
 uwsgi --socket :8000 --master --enable-threads --module agilos.wsgi
