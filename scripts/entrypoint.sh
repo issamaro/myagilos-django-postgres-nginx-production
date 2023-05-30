55d19cff320f76966f0b39b2d33fcd9457e737d4
issamaro <aissa.c@outlook.fr>
2023-05-31 00:05:32 +0200

    test

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Running wait_for_db script..." && python manage.py wait_for_db && echo "wait_for_db executed"
-echo "making migrations..." && python manage.py makemigrations
+# echo "making migrations..." && python manage.py makemigrations
 echo "trying to migrate..." && python manage.py migrate
 echo "collecting static files..." && python manage.py collectstatic --no-input
 echo "activating uwsgi..." && uwsgi --socket :8000 --master --enable-threads --module agilos.wsgi
